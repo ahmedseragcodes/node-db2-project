68c8463b32be0bd668a52c4e55b692269ebbfa3e
ahmedseragcodes <ahmedserag1@gmail.com>
2021-04-13 16:18:24 -0400

    successfully created table

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,1 +1,16 @@
-// DO YOUR MAGIC
+
+exports.up = function(knex) {
+    return knex.schema.createTable("cars", (table)=>{
+        table.increments().unsigned().primary();
+        table.text("vin").unique().notNullable();
+        table.text("make").notNullable();
+        table.text("model").notNullable();
+        table.integer("mileage").notNullable();
+        table.text("title");
+        table.text("transmission");
+    });
+};
+
+exports.down = function(knex){
+    return knex.schema.dropTableIfExists("cars");
+};
