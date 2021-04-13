c5bc884904d6888c728243225b51174d8c606f4f
ahmedseragcodes <ahmedserag1@gmail.com>
2021-04-13 16:34:03 -0400

    made seed and seeded table

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,14 @@
 // STRETCH
+exports.seed = function(knex){
+    //DELETES All Existing Entries
+    return knex("cars").truncate()
+    .then(()=>{
+        //Inserts Seed Entries
+        return knex("cars").insert([
+            {id: 1, vin: "1111", make: "chevorlet", model: "cobalt", mileage: "100,000"},
+            {id: 2, vin: "2222", make: "chevorlet", model: "malibu", mileage: "100,000"},
+            {id: 3, vin: "3333", make: "chevorlet", model: "corvette", mileage: "100,000"},
+            {id: 4, vin: "4444", make: "chevorlet", model: "silverado", mileage: "100,000"}
+        ]);
+    });
+};
