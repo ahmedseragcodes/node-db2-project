0914fa9f3f7a1630a62afa3fc37502e9ac6656ca
ahmedseragcodes <ahmedserag1@gmail.com>
2021-04-13 16:44:47 -0400

    wrote GET all router and model

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,6 +1,26 @@
 const express = require("express");
-const Cars = require("./cars-middleware");
+const Cars = require("./cars-model");
 
 const router = express.Router();
+
+//ENDPOINTS
+
+//[GET] All Cars
+router.get("/", (req, res, next)=>{
+    Cars.getAll()
+    .then((cars)=>{
+        console.log("SUCCEEDED GETTING ALL CARS", cars);
+        res.status(200).json(cars);
+    })
+    .catch((err)=>{
+        console.log("FAILED TO GET ALL CARS", err);
+    })
+});
+
+
+//ERROR CATCH ALL
+router.use((err, req, res, next)=>{
+    res.status(500).json({message: err.message});
+});
 
 module.exports = router;
