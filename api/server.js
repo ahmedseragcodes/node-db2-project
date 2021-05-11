f5abf4bb0d41d32f1345aa3095d5dd517d1600e1
ahmedseragcodes <ahmedserag1@gmail.com>
2021-05-11 19:00:31 -0400

    wrote server

--- api/server.js
+++ api/server.js
@@ -1,7 +1,24 @@
-const express = require("express")
+const express = require("express");
+const CarsRouter = require("./cars/cars-router");
+const cors = require("cors");
+const helmet = require("helmet");
 
-const server = express()
+const server = express();
 
-// DO YOUR MAGIC
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+server.use("/api/cars", CarsRouter);
+
+//SANITY CHECK ENDPOINT 
+server.get("*", (req, res, next)=>{
+    res.json({message: "API Up"});
+})
+
+//Error Handling Middleware 
+server.use((err, req, res, next)=>{
+    res.status(500).json({message: err.message});
+})
+
 
 module.exports = server
